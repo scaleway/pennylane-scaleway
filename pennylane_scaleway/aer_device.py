15789a8d8d9d32ab88c4988ed8f3d0297c0a41e8
smaugdela <simon.magdelaine.etu@gmail.com>
2025-10-27 17:17:01 +0100

    WIP implementing MVP.

--- pennylane_scaleway/aer_device.py
+++ pennylane_scaleway/aer_device.py
@@ -1,9 +1,24 @@
+# Copyright 2025 Scaleway
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 import pennylane as qml
 from pennylane.devices import Device, ExecutionConfig
 from pennylane.tape import QuantumScript, QuantumScriptOrBatch
 
 from qiskit_scaleway import ScalewayProvider
+from qiskit_scaleway.backends import AerBackend
 
 
 class AerDevice(Device):
@@ -31,14 +46,24 @@ class AerDevice(Device):
         self._provider = ScalewayProvider(
             project_id=kwargs.get("project_id"),
             secret_key=kwargs.get("secret_key"),
-            api_url=kwargs.get("api_url"),
+            url=kwargs.get("url"),
         )
+
+        backends = [backend for backend in self._provider.backends() if isinstance(backend, AerBackend)]
+        if self._instance not in [backend.name for backend in backends]:
+            raise ValueError(f"Backend {self._instance} not found. Available backends are {[backend.name for backend in backends]}.")
 
         self._backend = self._provider.get_backend(self._instance)
         if self._backend.availability != "available":
-            raise RuntimeError(f"Backend {self._instance} is not available")
+            raise RuntimeError(f"Backend {self._instance} is not available. Please try again later, or check availability at https://console.scaleway.com/qaas/sessions/create.")
 
-        self._session_id = self._backend.start_session(name="all-you-need-is-love")
+        self._session_id = self._backend.start_session(name="all-you-need-is-love", deduplication_id="all-you-need-is-love")
+
+    def preprocess_transforms(self, execution_config = None):
+        return super().preprocess_transforms(execution_config)
+
+    def setup_execution_config(self, config = None, circuit = None):
+        return super().setup_execution_config(config, circuit)
 
     def execute(
         self,
@@ -47,15 +72,44 @@ class AerDevice(Device):
     ):
         return 0.0 if isinstance(circuits, qml.tape.QuantumScript) else tuple(0.0 for c in circuits)
 
-    def __del__(self):
+    def stop(self):
         self._backend.stop_session(self._session_id)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
 
 
 if __name__ == "__main__":
-    dev = AerDevice()
+    import os
 
-    @qml.qnode(dev)
-    def circuit():
-        return qml.state()
+    with AerDevice(
+            project_id=os.environ["SCW_PROJECT_ID"],
+            secret_key=os.environ["SCW_SECRET_KEY"],
+            url=os.getenv("SCW_API_URL"),
+            instance=os.getenv("SCW_AER_INSTANCE", "aer_simulation_local"),
+        ) as dev:
 
-    print(circuit())
+        ###
+
+        @qml.qnode(dev)
+        def circuit():
+            return qml.state()
+
+        print(circuit())
+
+        ###
+
+        initial_config = ExecutionConfig()
+        initial_circuit_batch = [circuit]
+
+        execution_config = dev.setup_execution_config(initial_config)
+        transform_program = dev.preprocess_transforms(execution_config)
+        circuit_batch, postprocessing = transform_program(initial_circuit_batch)
+        results = dev.execute(circuit_batch, execution_config)
+        final_results = postprocessing(results)
+        print(final_results)
+
+        ###
